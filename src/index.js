c9aa8bf55db64a0cb57b907592d614263a6870d6
sangatification <rokayasangat123@gmail.com>
2024-01-10 21:37:37 +0545

    dotenv setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 // Install mongoose- npm i mongoose
+// Install dotenv - npm i dotenv
 
 const express = require('express')
+require('dotenv').config()
 const app = express()
-const port = 5000
+const port = process.env.PORT
 app.use(express.json()) //function that converts stringified body object into json
 const connection = require('./db/connection')
 const User = require('./models/user')
